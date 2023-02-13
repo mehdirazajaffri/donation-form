4175c613ee85a187064b809732477d8c974aec64
Mehdi Raza <mr@berkeley-assets.com>
2023-02-13 22:47:17 +0400

    Project Setup

---
+++ src/layouts/donations/index.js
@@ -0,0 +1,31 @@
+// @asseinfo/react-kanban components
+
+// react-html-parser components
+
+// uuid is a library for generating unique id
+
+// @mui material components
+
+// Material Dashboard 2 PRO React components
+import MDBox from "components/MDBox";
+
+// Material Dashboard 2 PRO React examples
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+
+// Kanban application components
+
+function Donations() {
+  return (
+    <DashboardLayout>
+      <MDBox py={3}>
+        <MDBox display="flex" justifyContent="center">
+          <MDBox width="100%" maxWidth={1200}>
+            <h1>Mehdi Raza</h1>
+          </MDBox>
+        </MDBox>
+      </MDBox>
+    </DashboardLayout>
+  );
+}
+
+export default Donations;
---
+++ src/routes.js
@@ -0,0 +1,168 @@
+/**
+=========================================================
+* Material Dashboard 2 PRO React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-pro-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+/** 
+  All of the routes for the Material Dashboard 2 PRO React are added here,
+  You can add a new route, customize the routes and delete the routes here.
+
+  Once you add a new route on this file it will be visible automatically on
+  the Sidenav.
+
+  For adding a new route you can follow the existing routes in the routes array.
+  1. The `type` key with the `collapse` value is used for a route.
+  2. The `type` key with the `title` value is used for a title inside the Sidenav. 
+  3. The `type` key with the `divider` value is used for a divider between Sidenav items.
+  4. The `name` key is used for the name of the route on the Sidenav.
+  5. The `key` key is used for the key of the route (It will help you with the key prop inside a loop).
+  6. The `icon` key is used for the icon of the route on the Sidenav, you have to add a node.
+  7. The `collapse` key is used for making a collapsible item on the Sidenav that contains other routes
+  inside (nested routes), you need to pass the nested routes inside an array as a value for the `collapse` key.
+  8. The `route` key is used to store the route location which is used for the react router.
+  9. The `href` key is used to store the external links location.
+  10. The `title` key is only for the item with the type of `title` and its used for the title text on the Sidenav.
+  10. The `component` key is used to store the component of its route.
+*/
+
+// Material Dashboard 2 PRO React layouts
+import Billing from "layouts/pages/account/billing";
+import Invoice from "layouts/pages/account/invoice";
+import Settings from "layouts/pages/account/settings";
+import Charts from "layouts/pages/charts";
+import Notifications from "layouts/pages/notifications";
+import PricingPage from "layouts/pages/pricing-page";
+import AllProjects from "layouts/pages/profile/all-projects";
+import ProfileOverview from "layouts/pages/profile/profile-overview";
+import Timeline from "layouts/pages/projects/timeline";
+import RTL from "layouts/pages/rtl";
+import NewUser from "layouts/pages/users/new-user";
+import Widgets from "layouts/pages/widgets";
+
+// Material Dashboard 2 PRO React components
+
+// @mui icons
+import Icon from "@mui/material/Icon";
+
+// Images
+import Donations from "layouts/donations";
+
+const routes = [
+  {
+    type: "collapse",
+    name: "Applications",
+    key: "applications",
+    icon: <Icon fontSize="medium">apps</Icon>,
+    collapse: [
+      {
+        name: "Donations",
+        key: "donations",
+        route: "/donations",
+        component: <Donations />,
+      },
+    ],
+  },
+  { type: "title", title: "Pages", key: "title-pages" },
+  {
+    type: "collapse",
+    name: "Pages",
+    key: "pages",
+    icon: <Icon fontSize="medium">image</Icon>,
+    collapse: [
+      {
+        name: "Profile",
+        key: "profile",
+        collapse: [
+          {
+            name: "Profile Overview",
+            key: "profile-overview",
+            route: "/pages/profile/profile-overview",
+            component: <ProfileOverview />,
+          },
+          {
+            name: "All Projects",
+            key: "all-projects",
+            route: "/pages/profile/all-projects",
+            component: <AllProjects />,
+          },
+        ],
+      },
+      {
+        name: "Users",
+        key: "users",
+        collapse: [
+          {
+            name: "New User",
+            key: "new-user",
+            route: "/pages/users/new-user",
+            component: <NewUser />,
+          },
+        ],
+      },
+      {
+        name: "Account",
+        key: "account",
+        collapse: [
+          {
+            name: "Settings",
+            key: "settings",
+            route: "/pages/account/settings",
+            component: <Settings />,
+          },
+          {
+            name: "Billing",
+            key: "billing",
+            route: "/pages/account/billing",
+            component: <Billing />,
+          },
+          {
+            name: "Invoice",
+            key: "invoice",
+            route: "/pages/account/invoice",
+            component: <Invoice />,
+          },
+        ],
+      },
+      {
+        name: "Projects",
+        key: "projects",
+        collapse: [
+          {
+            name: "Timeline",
+            key: "timeline",
+            route: "/pages/projects/timeline",
+            component: <Timeline />,
+          },
+        ],
+      },
+      {
+        name: "Pricing Page",
+        key: "pricing-page",
+        route: "/pages/pricing-page",
+        component: <PricingPage />,
+      },
+      { name: "RTL", key: "rtl", route: "/pages/rtl", component: <RTL /> },
+      { name: "Widgets", key: "widgets", route: "/pages/widgets", component: <Widgets /> },
+      { name: "Charts", key: "charts", route: "/pages/charts", component: <Charts /> },
+      {
+        name: "Notfications",
+        key: "notifications",
+        route: "/pages/notifications",
+        component: <Notifications />,
+      },
+    ],
+  },
+  { type: "divider", key: "divider-1" },
+];
+
+export default routes;
